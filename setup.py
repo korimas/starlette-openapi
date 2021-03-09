017a96a1a5a4ccdf612050b18fdc3f3677efd04c
zpzhou <himoker@163.com>
2021-03-09 13:44:00 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='starlette_openapi',
-    version='0.1',
+    version='0.11',
     author='zpzhou',
     author_email='himoker@163.com',
     url='https://github.com/zpdev/starlette-openapi',
